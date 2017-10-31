33dfce68160d0c240ec3ed9546c6576580df7f67
Will233 <s2573864@163.com>
2017-10-31 23:13:40 +0800

    添加token验证拦截

--- app/router.js
+++ app/router.js
@@ -1,12 +1,12 @@
 const Route = require('koa-router')
 const router = new Route()
-
+const {checkToken} = require('./lib/jwt.js')
 // 引入不同的controller
 const userController = require('./controller/userController')
 const dataController = require('./controller/dataController')
 
 // login
 router.post('/login', userController.Login)
-// get data
-router.get('/data', dataController.GetData)
+// get data, 验证token
+router.get('/data', checkToken, dataController.GetData)
 module.exports = router
