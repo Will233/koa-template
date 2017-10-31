e4216cfc33d63baea25157069fb09a8ea36614f7
Will233 <s2573864@163.com>
2017-10-31 23:13:03 +0800

    init

---
+++ app/service/cookieService.js
@@ -0,0 +1,22 @@
+/**
+ * cookie 的操作，cookie 相关的验证,
+ * 存储jwt
+ */
+
+ const getCookie = () => {
+
+ }
+
+ const setCookie = () => {
+
+ }
+
+ const delCookie = () => {
+
+ }
+
+ module.exports = {
+  getCookie,
+  setCookie,
+  delCookie
+ }
