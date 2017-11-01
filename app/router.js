07285a521e53bd9fc32f484ccca36adc1c479b45
Will233 <s2573864@163.com>
2017-11-01 15:55:44 +0800

    upodate router

--- app/router.js
+++ app/router.js
@@ -1,12 +1,14 @@
 const Route = require('koa-router')
 const router = new Route()
-const {checkToken} = require('./lib/jwt.js')
+const {checkToken} = require('./service/tokenService.js')
 // 引入不同的controller
 const userController = require('./controller/userController')
 const dataController = require('./controller/dataController')
 
 // login
 router.post('/login', userController.Login)
+router.get('/user', checkToken, userController.GetUser)
 // get data, 验证token
 router.get('/data', checkToken, dataController.GetData)
+router.get('/data/cookie', dataController.GetCookie)
 module.exports = router
